44936654c942a4f5eaa334e25e4de86a93b7004b
vs-chadh <daheecha0612@gmail.com>
2021-11-29 20:23:24 +0900

    commit: 싱글톤이 상태를 유지할 경우 생기는 문제점

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,35 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import static org.assertj.core.api.Assertions.*;
+
+public class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        // Thread A: A 사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+        // Thread B: B 사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        // 10000원 주문했지만, 20000원 출력 됨
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
